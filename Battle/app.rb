d83a69fd35271449128ecff37a3af85ca58fa7f9
RachaelMahon <rachmahon@gmail.com>
2016-10-12 00:06:39 +0100

    About to add the play route

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,16 +1,21 @@
 require 'sinatra/base'
 
 
+
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :index
   end
 
   post '/names' do
-
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
     erb :play
+  end
+
+  get '/play' do
+    
   end
 
   run! if app_file == $0
--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -2,5 +2,9 @@
 feature 'enter_names' do
   scenario 'visiting web page' do
     visit('/')
+    fill_in :player_1_name, with: "Buddy"
+    fill_in :player_2_name, with: "Snoopy"
+    click_button 'Play!'
+    expect(page).to have_content 'Buddy vs. Snoopy'
   end
 end
