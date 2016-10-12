5a55f2754818b3957118ff8aaa3fe4c6468b7291
RachaelMahon <rachmahon@gmail.com>
2016-10-12 15:32:13 +0100

    Moving on to step 20 after vigourous debugging

--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -1,10 +1,20 @@
 
 feature 'enter_names' do
-  scenario 'visiting web page' do
+
+  before :each do
     visit('/')
     fill_in :player1, with: "Buddy"
     fill_in :player2, with: "Snoopy"
+  end
+
+  scenario 'visiting web page' do
     click_button 'Play!'
     expect(page).to have_content 'Buddy vs. Snoopy'
+  end
+
+
+  scenario 'players can see each others hit points' do
+    visit ('/play')
+    expect(page).to have_text "#{@player1_HP}"
   end
 end
