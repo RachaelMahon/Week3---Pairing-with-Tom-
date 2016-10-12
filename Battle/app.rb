b086fd25562b228ed864ad9cae327a76951152e6
RachaelMahon <rachmahon@gmail.com>
2016-10-12 13:40:31 +0100

    Commits before debug

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,22 +1,45 @@
 require 'sinatra/base'
 
-
-
 class Battle < Sinatra::Base
   enable :sessions
+
   get '/' do
     erb :index
   end
 
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    erb :play
-  end
+  # post '/names' do
+  #   session[:player_1_name] = params[:player_1_name]
+  #   session[:player_2_name] = params[:player_2_name]
+  #   p params[:player_1_name]
+  #   p session[:player_2_name]
+  #   redirect '/play'
+  # end
+  #
+  # get '/play' do
+  #   @player_1_name = session[:player_1_name]
+  #   @player_2_name = session[:player_2_name]
+  #   p session[:player_1_name]
+  #   p params[:player_1_name]
+  #   p @player_1_name
+  #   erb(:play)
+  # end
 
-  get '/play' do
-    
-  end
+post '/names' do
+  session[:player1] = params[:player1]
+  session[:player2] = params[:player2]
+  p session[:player1]
+  p params[:player1]
+  redirect '/play'
+end
+
+get '/play' do
+  @player1 = params[:player1]
+  @player2 = params[:player1]
+  p session[:player1]
+  p params[:player1]
+  p @player2
+  erb(:play)
+end
 
   run! if app_file == $0
 end
--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -2,8 +2,8 @@
 feature 'enter_names' do
   scenario 'visiting web page' do
     visit('/')
-    fill_in :player_1_name, with: "Buddy"
-    fill_in :player_2_name, with: "Snoopy"
+    fill_in :player1, with: "Buddy"
+    fill_in :player2, with: "Snoopy"
     click_button 'Play!'
     expect(page).to have_content 'Buddy vs. Snoopy'
   end
